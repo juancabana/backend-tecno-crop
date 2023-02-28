0daf4e75fe58f77a9ee14c7ddea4d6a937434611
juancabana <juand.cabanat@unac.edu.co>
2023-02-28 14:53:50 -0500

    routing posts finished

--- src/controllers/posts/posts.controller.ts
+++ src/controllers/posts/posts.controller.ts
@@ -1,4 +1,67 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  // Get posts
+  @Get()
+  findAll(): object {
+    return {
+      message: 'Obtener posts',
+    };
+  }
+  // Get post
+  @Get(':id')
+  findOne(): object {
+    return {
+      message: 'Obtener post by id',
+    };
+  }
+
+  // Get posts user
+  @Get('user/:id')
+  getPostsUser(): object {
+    return {
+      message: 'Obtener posts de un usuario',
+    };
+  }
+
+  // Get favourites posts
+  @Get('favourites/:id')
+  getFavourites(): object {
+    return {
+      message: 'Obtener favoritos',
+    };
+  }
+
+  // Crear post
+  @Post()
+  create(): object {
+    return {
+      message: 'Create post',
+    };
+  }
+
+  // Update description post
+  @Patch('description/:id')
+  updateDescription(): object {
+    return {
+      message: 'Update description post',
+    };
+  }
+
+  // Update likes post
+  @Patch('like/:id')
+  updateLike(): object {
+    return {
+      message: 'Update like post',
+    };
+  }
+
+  // Delete post
+  @Delete(':id')
+  delete(): object {
+    return {
+      message: 'Delete post',
+    };
+  }
+}
