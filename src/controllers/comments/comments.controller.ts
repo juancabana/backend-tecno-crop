f78d0ed0bc0d20dd67edfc06663a30ca3f2ce38c
juancabana <juand.cabanat@unac.edu.co>
2023-03-01 11:29:55 -0500

    Routing comments finished

--- src/controllers/comments/comments.controller.ts
+++ src/controllers/comments/comments.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Put,
+  Post,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+  // Get all comments related to a post
+  @Get(':id')
+  findAll(): object {
+    return {
+      message: 'Obtener todos los comentarios de un post',
+    };
+  }
+
+  // Create new comment
+  @Post(':id')
+  create(): object {
+    return {
+      message: 'Create new comment',
+    };
+  }
+
+  // Update comment
+  @Put(':id')
+  update(): object {
+    return {
+      message: 'Update comment',
+    };
+  }
+
+  // Delete comment
+  @Delete(':id')
+  delete(): object {
+    return {
+      message: 'Delete comment',
+    };
+  }
+}
